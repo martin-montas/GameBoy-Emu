742f7a714ee748b5b9aba7898306c271ac9beb86
martin-montas <martinmontas1@gmail.com>
2026-06-25 19:58:56 -0400

    COMMIT: Now displaying and scrolling but with bad letters

--- src/ppu.cpp
+++ src/ppu.cpp
@@ -41,36 +41,28 @@ void Ppu::bg_update_framebuff(uint16_t addr) {
     bool    windown_v_trigger = LY >= _wy;
     bool    win_used          = false;
 
-    for (int t = 0; t < 19; t++) {
-        // if (windown_v_trigger && (t >= _wx - 7)) {
-        //     if (!(LCDC & FLAG_WIN_ENABLE)) {
-        //         continue;
-        //     }
-        //     uint8_t win_x = t - (_wx - 7);
-        //     uint8_t win_y = _win_line;
-        //     win_used      = true;
-        // }
-        // else {
+    for (int t = 0; t <= 19; t++) {
         uint8_t _scy = _mmu->read8(0xFF42);
         uint8_t _scx = _mmu->read8(0xFF43);
 
         uint8_t background_x = (_scx + t);
         uint8_t background_y = (LY + _scy);
 
-        int start_tile_x = background_x / 8;
+        // int start_tile_x = background_x / 8;
         int start_tile_y = background_y / 8;
 
         /* gets tile index out of tile map */
-        uint8_t tile_index = _mmu->read8(addr + (32 * start_tile_y) + start_tile_x);
+        uint8_t tile_index = _mmu->read8(addr + (32 * start_tile_y) + t);
 
         /* signed 8 bit integer for the indexing tile data */
         uint16_t tile_data;
 
-        if ((LCDC & FLAG_BG_AREA) == 0) {
-            tile_data = 0x8000 + (tile_index * 16);
-        } else {
-            tile_data = 0x9000 + ((int8_t)tile_index * 16);
-        }
+        /* gets tile data based on the indexing method  in LCDC */
+        // if ((LCDC & FLAG_BG_AREA) == 0) {
+        tile_data = 0x8000 + (tile_index * 16);
+        // } else {
+        // tile_data = 0x9000 + (tile_index * 16);
+        // }
         uint8_t tile_row = background_y % 8;
         uint8_t lsb      = _mmu->read8(tile_data + (2 * tile_row));
         uint8_t msb      = _mmu->read8(tile_data + (2 * tile_row) + 1);
@@ -92,111 +84,6 @@ void Ppu::bg_update_framebuff(uint16_t addr) {
             frame_buff[LY * WIDTH + (t * 8) + k] = color_val;
         }
     }
-
-    // for (int x = 0; x <= 159; x++) {
-    //     if (windown_v_trigger && (x >= _wx - 7)) {
-    //         if (!(LCDC & FLAG_WIN_ENABLE)) {
-    //             continue;
-    //         }
-    //         uint8_t win_x = x - (_wx - 7);
-    //         uint8_t win_y = _win_line;
-    //         win_used      = true;
-
-    //         /* used to get which tile the curent pixel belongs to */
-    //         int tile_x = win_x / 8;
-    //         int tile_y = win_y / 8;
-
-    //         /* get tile map index with offset */
-    //         int    offset = (tile_y * 32) + tile_x;
-    //         int8_t tile_index;
-
-    //         /* fetches window tile  map index with offset */
-    //         tile_index = _mmu->read8(addr + offset);
-
-    //         uint16_t tile_data_addr;
-
-    //         if ((LCDC & FLAG_BG_AREA) == 0) {
-    //             tile_index     = (uint8_t)tile_index;
-    //             tile_data_addr = _mmu->read8(0x8000 + (tile_index * 16));
-    //         } else {
-    //             tile_data_addr = _mmu->read8(0x9000 + (tile_index * 16));
-    //         }
-
-    //         int pixel_y = win_y % 8;
-    //         int pixel_x = win_x % 8;
-
-    //         uint8_t byte0 = _mmu->read8(tile_data_addr + (pixel_x * 2));
-    //         uint8_t byte1 = _mmu->read8(tile_data_addr + (pixel_y * 2) + 1);
-
-    //         bool msb = ((byte0 >> (7 - win_y)) & 1);
-    //         bool lsb = ((byte1 >> (7 - win_x)) & 1);
-
-    //         uint8_t  color = (msb << 1) | lsb;
-    //         uint32_t color_val;
-    //         if (color == 0) {
-    //             color_val = WHITE;
-    //         } else if (color == 1) {
-    //             color_val = LIGHT_GRAY;
-    //         } else if (color == 2) {
-    //             color_val = DARK_GRAY;
-    //         } else {
-    //             color_val = BLACK;
-    //         }
-
-    //         frame_buff[_win_line * WIDTH + x] = color_val;
-    //     } else {
-    //         // TODO: make the background use the 2 addressing modes as well
-    //         //
-    //         uint8_t _scy         = _mmu->read8(0xFF42);
-    //         uint8_t _scx         = _mmu->read8(0xFF43);
-    //         uint8_t background_x = (x + _scx);
-    //         uint8_t background_y = (LY + _scy);
-    //         int     tile_x       = background_x / 8;
-    //         int     tile_y       = background_y / 8;
-
-    //         // fetches offset for the tile map
-    //         int offset = (tile_y * 32 + tile_x);
-
-    //         //  gets tile number from tile map
-    //         int8_t   tile_number = _mmu->read8(addr + offset);
-    //         uint16_t tile_data_addr;
-
-    //         if ((LCDC & FLAG_BG_AREA) == 0) {
-    //             tile_number    = (uint8_t)tile_number;
-    //             tile_data_addr = _mmu->read8(0x8000 + (tile_number * 16));
-    //         } else {
-    //             tile_data_addr = _mmu->read8(0x9000 + (tile_number * 16));
-    //         }
-
-    //         int pixel_y = background_y % 8;
-    //         int pixel_x = background_x % 8;
-
-    //         uint8_t byte0 = _mmu->read8(tile_data_addr + (pixel_y * 2));
-    //         uint8_t byte1 = _mmu->read8(tile_data_addr + (pixel_y * 2) + 1);
-
-    //         bool msb = ((byte0 >> (7 - pixel_x)) & 1);
-    //         bool lsb = ((byte1 >> (7 - pixel_x)) & 1);
-
-    //         uint8_t  color = (msb << 1) | lsb;
-    //         uint32_t color_val;
-    //         if (color == 0) {
-    //             color_val = WHITE;
-    //         } else if (color == 1) {
-    //             color_val = LIGHT_GRAY;
-    //         } else if (color == 2) {
-    //             color_val = DARK_GRAY;
-    //         } else {
-    //             color_val = BLACK;
-    //         }
-
-    //         frame_buff[LY * WIDTH + x] = color_val;
-    //     }
-    //     if (win_used) {
-    //         _win_line += 1;
-    //         win_used = false;
-    //     }
-    // }
-    //}
 }
 
 void Ppu::win_update_framebuff(uint16_t addr) {
@@ -207,21 +94,11 @@ void Ppu::win_update_framebuff(uint16_t addr) {
 
 void Ppu::enter_mode_3() {
     _LCDC = _mmu->read8(LCDC_ADDR);
-    // TODO: refactor this here
-    if (_f_flag == bg)
-        /* checks the bg map from _LCDC reg */
-        if (!(_LCDC & FLAG_BG_MAP)) {
-            bg_update_framebuff(0x9800);
-        } else {
-            bg_update_framebuff(0x9C00);
-        }
-    else
-        /* update window component */
-        if (!(_LCDC & FLAG_WIN_MAP)) {
-            win_update_framebuff(0x9800);
-        } else {
-            win_update_framebuff(0x9C00);
-        }
+    if (!(_LCDC & FLAG_BG_MAP)) {
+        bg_update_framebuff(0x9800);
+    } else {
+        bg_update_framebuff(0x9C00);
+    }
 }
 
 void Ppu::enter_mode_2() {

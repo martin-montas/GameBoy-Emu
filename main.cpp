cfe570215f1c1a8028a020d4a3d295f48bfc78dc
martin-montas <martinmontas1@gmail.com>
2026-07-06 02:22:54 -0400

    Deleted a minor an unused comment

--- main.cpp
+++ main.cpp
@@ -10,11 +10,5 @@ int main(int argc, char** argv) {
     GameBoy* game = new GameBoy(argv[1]);
     game->run();
     delete (game);
-
-    // for unit tests:
-
-    // TestRunner test;
-    // test.run_cpu_test(argv[1]);
-
     return 0;
 }
